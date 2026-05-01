6f9e4b161ff957cd46e09b554ac35c6f7ae5a665
NightSlayer7852 <186535326+NightSlayer7852@users.noreply.github.com>
2026-05-01 20:45:47 +0530

    Add: matchsticks-to-square - submission-0

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        total = sum(matchsticks)
+        if total % 4 != 0:
+            return False
+        n = len(matchsticks)
+        side = total // 4
+        matchsticks.sort(reverse = True)    
+        sides = [0] * 4
+        def backtrack(i):
+            if i == n:
+                return True
+            
+            for j in range(4):
+                if sides[j] + matchsticks[i] <= side:
+                    sides[j] += matchsticks[i]
+                    if backtrack(i + 1):
+                        return True
+                    sides[j] -= matchsticks[i]
+            return False
+        return backtrack(0)
